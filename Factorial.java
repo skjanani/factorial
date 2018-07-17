3a497f5a2f64cc39a9465166e4adebebd6d0ef94
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-17 13:58:57 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+package janani;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n=5,fact=1;
+for(i=1;i<=5;i++){
+fact=fact*i;
+}
+System.out.println("factorial of "+n+" is"+fact);
+	}
+
+}
